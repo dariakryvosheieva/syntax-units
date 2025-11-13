73646f2945a1a2f6ac1267a6ed607e483b377fb6
Daria Kryvosheieva <144752536+dariakryvosheieva@users.noreply.github.com>
2025-11-13 03:21:40 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=100G
 #SBATCH --gres=gpu:h100:1
 #SBATCH --partition=mit_normal_gpu
-python ablation.py --model-name openai-community/gpt2-xl --dataset blimp --ablation-type mean --savedir english/mean-ablation --percentage 1
+python ablation.py --model-name openai-community/gpt2-xl --dataset blimp --ablation-type mean --savedir english/mean-ablation --percentage 1.0
